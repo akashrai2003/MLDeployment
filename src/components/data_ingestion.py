b7679c482eee8d21c2517e7ae58c8c071a2655c9
college-akashrai <1nt21ec008.akash@nmit.ac.in>
2024-02-27 13:43:11 +0530

    Data transformation pipeline added

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,10 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+from src.components.data_transformation import DataTransformationConfig
+from src.components.data_transformation import DataTransformation
+
+
 @dataclass
 class DataIngestionConfig:
     '''def __init__(self,config):
@@ -109,4 +113,6 @@ class DataIngestion:
 
 if __name__=="__main__":
     obj=DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+    data_transformation_obj=DataTransformation()
+    data_transformation_obj.initiate_data_transformation(train_data,test_data)
